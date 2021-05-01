9812cfe2ad9c9e6631f026fb577e77f785970e43
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-05-01 19:18:15 +0100

    Include spaceover standardized documentation resources

---
+++ src/main/java/com/springboot/training/spaceover/spacemission/manager/SpaceMissionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.training.spaceover.spacemission.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceMissionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceMissionManagerApplication.class, args);
+	}
+
+}
