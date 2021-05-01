82abeb9f7ed872f57d0ae412d91798449b1fa5b7
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-05-01 19:58:54 +0100

    Included domain entity model

---
+++ src/main/java/com/springboot/training/spaceover/spacemission/manager/domain/model/SpaceMission.java
@@ -0,0 +1,45 @@
+package com.springboot.training.spaceover.spacemission.manager.domain.model;
+
+import com.springboot.training.spaceover.spacemission.manager.enums.SpaceMissionStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+public class SpaceMission extends Auditable<String> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @PositiveOrZero
+    private Long spaceShipId;
+
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String details;
+
+    @NotNull
+    private SpaceMissionStatus status;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal revenue;
+
+}
