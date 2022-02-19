c67a53feb666be610dcc323b33843058b897ee87
Bruno Caetano <bruno.caetano1@vodafone.com>
2022-02-19 22:00:42 +0000

    minor change

--- src/main/java/com/springboot/training/spaceover/spacemission/manager/configuration/SpaceShipClientConfiguration.java
+++ src/main/java/com/springboot/training/spaceover/spacemission/manager/configuration/SpaceShipClientConfiguration.java
@@ -1,11 +1,7 @@
 package com.springboot.training.spaceover.spacemission.manager.configuration;
 
-import com.springboot.training.spaceover.spacemission.manager.service.RestTemplateSpaceShipClient;
-import com.springboot.training.spaceover.spacemission.manager.service.SpaceShipClient;
-import com.springboot.training.spaceover.spacemission.manager.service.WebClientSpaceShipClient;
 import com.springboot.training.spaceover.spacemission.manager.utils.properties.SpaceMissionManagerProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,27 +15,16 @@ public class SpaceShipClientConfiguration {
     private final SpaceMissionManagerProperties spaceMissionManagerProperties;
 
     @Bean
-    @ConditionalOnProperty(name = "spaceship-manager.client.provider", havingValue = "rest-template", matchIfMissing = true)
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
 
-    @Bean
-    @ConditionalOnProperty(name = "spaceship-manager.client.provider", havingValue = "rest-template", matchIfMissing = true)
-    public SpaceShipClient restTemplateSpaceShipClient(final RestTemplate restTemplate, final SpaceMissionManagerProperties properties) {
-        return new RestTemplateSpaceShipClient(restTemplate, properties);
-    }
+
 
     @Bean
-    @ConditionalOnProperty(name = "spaceship-manager.client.provider", havingValue = "web-client")
     public WebClient webClient() {
         return WebClient.builder().baseUrl(spaceMissionManagerProperties.getSpaceshipManagerBaseUrl()).build();
     }
 
-    @Bean
-    @ConditionalOnProperty(name = "spaceship-manager.client.provider", havingValue = "web-client")
-    public SpaceShipClient webClientSpaceShipClient(final WebClient webClient) {
-        return new WebClientSpaceShipClient(webClient);
-    }
 
 }
