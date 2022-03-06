2b9aaa5ad23b5fff895747062986613c1470668a
Bruno Caetano <bruno.caetano1@vodafone.com>
2022-03-06 14:11:25 +0000

    included spring cloud dependencies

--- src/main/java/com/springboot/training/spaceover/spacemission/manager/SpaceMissionManagerApplication.java
+++ src/main/java/com/springboot/training/spaceover/spacemission/manager/SpaceMissionManagerApplication.java
@@ -2,7 +2,11 @@ package com.springboot.training.spaceover.spacemission.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpaceMissionManagerApplication {
 
---
+++ src/main/java/com/springboot/training/spaceover/spacemission/manager/service/OpenFeignSpaceShipClient.java
@@ -0,0 +1,16 @@
+package com.springboot.training.spaceover.spacemission.manager.service;
+
+import com.springboot.training.spaceover.spacemission.manager.domain.response.inbound.GetSpaceShipResponse;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("spaceship-manager")
+@ConditionalOnProperty(name = "spaceship-manager.client.provider", havingValue = "open-feign")
+public interface OpenFeignSpaceShipClient extends SpaceShipClient {
+
+    @GetMapping("spaceships/{id}")
+    GetSpaceShipResponse findBydId(@PathVariable("id") Long id);
+
+}
